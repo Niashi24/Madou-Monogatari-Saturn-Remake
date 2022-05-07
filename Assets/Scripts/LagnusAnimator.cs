65d57e75463660bfc12e2269f0a577932b8eb35f
Leoshe22 <leosparacino4@gmail.com>
2022-05-06 20:02:10 -0400

    Implemented Animations

---
+++ Assets/Scripts/LagnusAnimator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LS.Utilities;
+
+public class LagnusAnimator : MonoBehaviour
+{
+    [SerializeField]
+    LagnusMovement _player;
+
+    [SerializeField]
+    ValueReference<LagnusInput> _rawInput;
+
+    [SerializeField]
+    Animator _animator;
+
+    [SerializeField]
+    SpriteRenderer _spriteRenderer;
+
+    Vector2 currentDirection;
+
+    void Update()
+    {
+        if (_rawInput.Value.Direction != Vector2.zero)
+            currentDirection = _rawInput.Value.Direction;
+
+        var targetDirection = _player.Movement != Vector2.zero ? _player.Movement : currentDirection;
+
+        _animator.SetFloat("X", targetDirection.x);
+        _animator.SetFloat("Y", targetDirection.y);
+        _animator.SetBool("Moving", _rawInput.Value.Moving);
+
+        _spriteRenderer.flipX = targetDirection.x == 1;
+    }
+}
