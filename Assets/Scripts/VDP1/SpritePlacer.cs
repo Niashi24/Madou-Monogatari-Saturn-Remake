f55ac86e5e5b63d6b606b7e9a165da46fe485bfc
Leoshe22 <leosparacino4@gmail.com>
2022-06-09 15:23:35 -0400

    Refactored SpritePlacer to Disable and Enable Sprites as Needed

--- Assets/Scripts/VDP1/SpritePlacer.cs
+++ Assets/Scripts/VDP1/SpritePlacer.cs
@@ -7,68 +7,68 @@ using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class SpritePlacer : MonoBehaviour
+public class SpritePlacer : SerializedMonoBehaviour
 {
     [SerializeField]
     TextureDictionary _dict;
 
     [SerializeField]
-    List<SpriteRenderer> currentSprites = new();
+    Dictionary<string, SpriteRenderer> currentSpritesDictionary = new();
     
     [SerializeField]
     TextAsset _text;
-
-    ObjectPool<SpriteRenderer> spritePool = new(
-        () => {
-            var gameObj = new GameObject("Sprite");
-            return gameObj.AddComponent<SpriteRenderer>();
-        },
-        (x) => x.enabled = true,
-        (x) => x.enabled = false,
-        (x) => Destroy(x.gameObject)
-    );
 
     [Button]
     public void PlaceSprites()
     {
         var commands = DrawCommandFactory.GetCommandsFromDebug(_text);
 
-        currentSprites.ForEach(x => DestroyImmediate(x.gameObject));
-        currentSprites.Clear();
-        
-        int i = 0;
-        foreach (var command in commands)
+        foreach (var renderer in currentSpritesDictionary.Values)
         {
-            if (command is ScaledSpriteCommand scaledSprite)
+            renderer.gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i < commands.Count; i++)
+        {
+            if (commands[i] is ScaledSpriteCommand scaledSprite)
             {
-                if (_dict.addressSpriteDictionary.ContainsKey(scaledSprite.textureAddress))
+                var sprite = GetSprite(scaledSprite.textureAddress);
+                if (sprite is not null)
                 {
-                    var texture = _dict.addressSpriteDictionary[scaledSprite.textureAddress];
-                    PlaceSprite(
-                        texture,
-                        scaledSprite,
-                        i
-                    );
-                } else
-                {
-                    Debug.Log($"Missing texture: {scaledSprite.textureAddress}");
+                    PlaceSprite(sprite, scaledSprite, i);
                 }
             }
-            i++;
         }
     }
 
-    void PlaceSprite(Sprite texture, ScaledSpriteCommand scaledSprite, int num)
+    void PlaceSprite(SpriteRenderer renderer, ScaledSpriteCommand scaledSprite, int num)
     {
-        var gameObj = new GameObject(scaledSprite.textureAddress);
-        gameObj.transform.parent = transform;
-        var renderer = gameObj.AddComponent<SpriteRenderer>();
-        currentSprites.Add(renderer);
-
-        renderer.sprite = texture;
         renderer.transform.position = new Vector3(scaledSprite.xa, -scaledSprite.ya);
         renderer.flipX = scaledSprite.textureReadDirection != TextureReadDirection.Normal;
         renderer.sortingOrder = num;
+        renderer.gameObject.SetActive(true);
         // throw new NotImplementedException();
+    }
+
+    SpriteRenderer GetSprite(string textureAddress)
+    {
+        if (currentSpritesDictionary.ContainsKey(textureAddress))
+            return currentSpritesDictionary[textureAddress];
+        //not loaded
+        if (_dict.addressSpriteDictionary.ContainsKey(textureAddress))
+        {
+            var gameObj = new GameObject(textureAddress);
+            gameObj.transform.parent = transform;
+            var renderer = gameObj.AddComponent<SpriteRenderer>();
+            renderer.sprite = _dict.addressSpriteDictionary[textureAddress];
+
+            currentSpritesDictionary.Add(textureAddress, renderer);
+
+            return renderer;
+        }
+
+        Debug.Log($"Missing texture: {textureAddress}");
+
+        return null;
     }
 }
